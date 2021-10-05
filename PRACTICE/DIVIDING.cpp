389f07fc95f64e39dc908a922b1cbc333d793678
Pawanesh <55928870+Pawaneshmishra@users.noreply.github.com>
2021-10-06 02:01:33 +0530

    DIVIDING.cpp

---
+++ PRACTICE/DIVIDING.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long int n,sum,x;
+    
+    cin>>n;
+    sum=0;
+    
+    for(long long int i=0; i<n; i++)
+    {
+        cin>>x;
+        sum+=x;
+    }
+    
+    if(((n*(n+1))/2)==sum)  cout<<"YES\n";
+    else cout<<"NO\n";
+    
+    return 0;
+}
